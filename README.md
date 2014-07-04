1fc55248f46431d5355aa3faae764153fef1d0af
themarkappleby <themarkappleby@gmail.com>
2014-07-04 11:20:25 -0400

    merged mustache template branch

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-guidedog
+Guidedog
 ========
 
 Create platform agnostic living styleguides with ease
+
+##What is this thing?
+
+Front-end frameworks like [Bootstrap](http://getbootstrap.com/) and [Foundation](http://foundation.zurb.com/) are awesome. The most awesome feature of these frameworks is not their plethora of shiny components but instead their thorough documentation. Having extensive front-end documentation helps prevent developers from writing redundant code, makes it easier to on-board new developers to a project, and ensures better communication between front-end and back-end devs.
+
+As awesome as front-end frameworks are, sometimes you just want to roll your own and that's where Guidedog comes in. Guidedog is a back-end agnostic javascript plugin that will parse your stylesheets for comments and automatically create beautiful documentation.
+
+##Getting Started
+
+The recommended way to install Guidedog is via [Bower](http://bower.io/) but Guidedog can also be install without Bower as well if you prefer (see below). 
+
+###Installing Guidedog with Bower
+
+1. Fetch the plugin via bower `bower install guidedog`. Note - by default Bower installs components in a folder names bower_components in your project's root. If you'd like to change the default install location see the [bowerrc documentation](http://bower.io/docs/config/). Ensure that bower installs Guidedog in a location that is publicly accessible.
+1. Create a new page/view and add `<script data-guidedog-path='assets/application.css' src='bower_components/guidedog/guidedog.js' type='text/javascript'></script>` replacing `data-guidedog-path` with the path to your concatenated and minified stylesheet.
--- guidedog.js
+++ guidedog.js
@@ -1,137 +1,82 @@
 var Styleguide = function() {
 
   var sg = this 
+  var baseUrl = ''
 
-  sg.data = []
-  sg.painters = []
+  sg.data = {} 
+  sg.data.sections = [] 
+
+  // load all dependencies
+  var load_assets = function(){
+    var html = ''
+    baseUrl = $('[data-guidedog-path]').attr('src').replace('guidedog.js', '') 
+    html += '<link rel="stylesheet" type="text/css" href="'+baseUrl+'/lib/default.min.css" />'
+    html += '<link rel="stylesheet" type="text/css" href="'+baseUrl+'/lib/googlecode.min.css" />'
+    html += '<link rel="stylesheet" type="text/css" href="'+baseUrl+'/lib/guidedog.css" />'
+    html += '<script type="text/javascript" src="'+baseUrl+'/lib/highlight.min.js" />'
+    html += '<script type="text/javascript" src="'+baseUrl+'/lib/js-yaml.js" />'
+    html += '<script type="text/javascript" src="'+baseUrl+'/lib/markdown.js" />'
+    html += '<script type="text/javascript" src="'+baseUrl+'/lib/mustache.js" />'
+    $('head').append(html)
+  }
 
   // extract styleguide comments from stylesheet
   sg.init = function(s){
     load_assets()
     $.when($.get(s)).done(function(response) {
       var expression = /\/\*\!\!\!([\s\S]*?)\*\//mg
-      var match
       while ((match = expression.exec(response)) != null){
-        match = parse_yaml(scrub_comments(match[1]))
+        match = jsyaml.load(scrub_comments(match[1]))
         if (new_section(match.section)){
-          sg.data[match.section] = new Array()
+          var section = match.section
+          if(!match.section) match.section = 'Undefined'
+          sg.data.sections.push({
+            "section": match.section, 
+            "id": match.section.replace(/\s+/g, '').toLowerCase(), 
+            "subSections": []
+          })
         }
-        sg.data[match.section][match.title] = match
+        var index = sectionIndex(match.section)
+        sg.data.sections[index].subSections.push(match)
       }
       render()
     });
   }
 
-  // load all dependencies
-  var load_assets = function(){
-    var target = $('[data-guidedog-path]')
-    var url = target.attr('src')
-    var base_url = url.replace('guidedog.js', '') 
-    var html = ''
-    html += '<link rel="stylesheet" type="text/css" href="'+base_url+'/lib/default.min.css" />'
-    html += '<link rel="stylesheet" type="text/css" href="'+base_url+'/lib/googlecode.min.css" />'
-    html += '<link rel="stylesheet" type="text/css" href="'+base_url+'/lib/guidedog.css" />'
-    html += '<script type="text/javascript" src="'+base_url+'/lib/highlight.min.js" />'
-    html += '<script type="text/javascript" src="'+base_url+'/lib/js-yaml.js" />'
-    html += '<script type="text/javascript" src="'+base_url+'/lib/markdown.js" />'
-    $('head').append(html)
-  }
-
-  // loop through and output data as html
+  // pass json data to Mustache template
   var render = function(){
-    $('body').trigger('preRender')
-    var html = ''
-    for(section in sg.data){
-      html += '<section class="sg"><a id="'+section+'"></a><h2 class="sg">'+section+'</h2>'  
-      for(section_item in sg.data[section]){
-        section_item = sg.data[section][section_item]
-        html += '<div class="sg-item">'
-        for(key in section_item){
-          html += sg.painters[key](section_item[key])
-        }
-        html += '</div>'
-      }
-      html += '</section>'  
-    }
-    html += nav()
-    $('body').html(html).trigger('postRender')
-  }
-
-  // generate navigation
-  var nav = function(){
-    var html = '<nav class="sg">'
-    for(section in sg.data){
-      html += '<a href="#'+ section +'">' + section + '</a>'
-    }
-    html += '</nav>'
-    return html 
-  }
-
-  // render titles
-  sg.painters.title = function(s){
-    return '<h3 class="sg">'+s+'</h3>'
-  }
-
-  // render sections
-  sg.painters.section = function(s){
-    return ''
-  }
-
-  // render descriptions
-  sg.painters.description = function(s){
-    return markdown.toHTML(s)
-  }
-
-  // render swatches
-  sg.painters.swatches = function(s){
-    var output = '<div class="sg-swatches">'
-    for (key in s){
-      value = s[key]
-      output += '<div class="sg-swatch">'
-      output += '<div class="sg-swatch-sample" style="background-color:#'+value+'">'
-      output += '</div>'
-      output += '<div class="sg-swatch-info">'
-      output += '<span class="sg-swatch-key">' + key + '</span> <span class="sg-swatch-value">#' + value + '</span>'
-      output += '</div>'
-      output += '</div>'
-    }
-    output += '</div>'
-    return output 
-  }
-
-  // render example code
-  sg.painters.example = function(s){
-    var code = $('<span/>').text(s).html()
-    code = '<pre><code>'+code+'</code></pre>'
-    s = '<div class="sg-code-output">' + s + '</div>'
-    return '<div class="sg-code">' + s + code + '</div>'
-  }
-  $(document).ready(function(){
-    $('body').on('postRender', function(){
+    $.get(baseUrl+'/lib/view.html', function(template) {
+      console.log(sg.data)
+      $('body').append(Mustache.render(template, sg.data))
       $('pre code').each(function(i, e) {hljs.highlightBlock(e)})
-    })
-  });
+    });
+  }
 
-  // determine if the section in question already exists
+  // fetch the index of a section
+  var sectionIndex = function(match){
+    for (var i=0; i<sg.data.sections.length; i++){
+      if(sg.data.sections[i].section === match){
+        return i
+      }
+    }
+    return 0
+  }
+
+  // determine if a section already exists
   var new_section = function(section){
-    for (key in sg.data){
-      if(section === key){
+    for (key in sg.data.sections){
+      if(section === sg.data.sections[key].section){
         return false
       }
     }
     return true
   }
 
-  // prep string for YAML parser but cleaning up white-space and comment marks
+  // prep string for YAML parser
   var scrub_comments = function(match){
     match = match.replace(/\ *\*\ /g, '')
     match = match.replace(/\#/g, '')
     return match
-  }
-
-  // parse string as YAML
-  var parse_yaml = function(match){
-    return jsyaml.load(match)
   }
 }
 var sg = new Styleguide()
